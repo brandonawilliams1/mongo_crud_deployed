6d3ea907080959c5695bba204cdc50b41e0a0c9f
Brandon Williams <brandonawilliams1@gmail.com>
2022-02-05 14:04:26 -0600

    added uri variable to the connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 const { connect, connection } = require('mongoose');
 
+const uri =
+process.env.MONGODB_URI;
+
 connect('mongodb://localhost/videosAndResponses', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
